c5c048b6f4bd98518193a90c209831404776f9b8
daiysg <daiysg91@gmail.com>
2017-10-12 22:50:38 +0800

    create pcap4j class

--- src/SmartGridBillingSenario/Pcap4jExample.java
+++ src/SmartGridBillingSenario/Pcap4jExample.java
@@ -1,13 +1,12 @@
 package SmartGridBillingSenario;
 
+import SmartGridBillingSenario.attack.Pcap4j;
 import SmartGridBillingSenario.utils.Utils;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.binary.Hex;
-import org.pcap4j.core.*;
-import org.pcap4j.packet.BsdLoopbackPacket;
-import org.pcap4j.packet.IpV4Packet;
+import org.pcap4j.core.NotOpenException;
+import org.pcap4j.core.PcapNativeException;
 import org.pcap4j.packet.Packet;
-import org.pcap4j.packet.TcpPacket;
 
 import java.io.EOFException;
 import java.net.UnknownHostException;
@@ -19,87 +18,25 @@ import java.util.concurrent.TimeoutException;
 @Slf4j
 public class Pcap4jExample {
 
-    private static PcapHandle openPcap(String networkInterfaceName, int snaplen) {
-        try {
-            PcapNetworkInterface nif = Pcaps.getDevByName(networkInterfaceName);
-            if (nif == null) {
-                throw new RuntimeException("Couldn't open network interface " +
-                        networkInterfaceName);
-            } else {
-                log.info(
-                        "Forward network traffic from " + nif.getName() + "(" +
-                                nif.getAddresses() + ")");
-            }
-            return nif.openLive(snaplen,
-                    PcapNetworkInterface.PromiscuousMode.PROMISCUOUS, 10);
-        } catch (PcapNativeException e) {
-            log.error(
-                    "Couldn't open network interface " + networkInterfaceName,
-                    e);
-            throw new RuntimeException(e);
-        }
-    }
-
     public static void main(String[] args) throws UnknownHostException, PcapNativeException, EOFException, TimeoutException, NotOpenException {
 
-        String deviceName = "lo0";
-        // 抓取包长度
-        int snaplen = 64 * 1024;
+        String host = "192.168.0.154";
+        Pcap4j pcap4j = new Pcap4j(host) {
+            @Override
+            public void handleTcpData(String srcAddr, String dstAddr, String srcPort, String dstPort, Packet payload) {
+                byte[] tcpRawData = payload.getRawData();
+                if (tcpRawData.length > 4) {
 
-        PcapHandle handle = openPcap(deviceName, snaplen);
-
-        String filter = "ip and tcp and (src host 192.168.0.154)";
-
-        // 设置过滤器
-        handle.setFilter(filter, BpfProgram.BpfCompileMode.OPTIMIZE);
-        runPcapToDispatcher(handle);
-    }
-
-    private static void runPcapToDispatcher(PcapHandle pcapHandle) {
-        Thread pcapThread = new Thread() {
-            public void run() {
-                pcapToDispatcher(pcapHandle);
+                    log.info("Src = {}:{}", srcAddr, srcPort);
+                    log.info("Dst = {}:{}", dstAddr, dstPort);
+                    String hexValue = Hex.encodeHexString(tcpRawData);
+                    String value = Utils.convertHexToString(hexValue).substring(3);
+                    log.info("Package Aquired, value: {}", value);
+                }
             }
         };
 
-        pcapThread.start();
-    }
-
-    private static void pcapToDispatcher(PcapHandle pcapHandle) {
-        while (true) {
-            try {
-                Packet packet = pcapHandle.getNextPacketEx();
-                if (packet != null) {
-                    packet.getHeader();
-                    byte[] rawData = packet.getRawData();
-                    if (rawData.length < 14) {
-                        continue;
-                    }
-
-                    IpV4Packet ipV4Packet = (IpV4Packet) ((BsdLoopbackPacket) packet).getPayload();
-                    TcpPacket tcpPacket = (TcpPacket) ipV4Packet.getPayload();
-
-
-                    // means have more data then header
-                    if (tcpPacket.getPayload() != null) {
-
-                        byte[] tcpRawData = tcpPacket.getPayload().getRawData();
-                        if (tcpRawData.length > 4) {
-                            log.info("TCP, dataoffset {}", tcpRawData.length);
-                            String hexValue = Hex.encodeHexString(tcpRawData);
-                            log.info("Data with Hex value {}", hexValue);
-                            String value = Utils.convertHexToString(hexValue).substring(3);
-
-                            log.info("Package Aquired, value: {}", value);
-                        }
-                    }
-                }
-            } catch (PcapNativeException | EOFException | TimeoutException e) {
-                continue;
-            } catch (NotOpenException e) {
-                e.printStackTrace();
-            }
-        }
+        pcap4j.startCapture();
     }
 }
 
---
+++ src/SmartGridBillingSenario/attack/Pcap4j.java
@@ -0,0 +1,114 @@
+package SmartGridBillingSenario.attack;
+
+import lombok.extern.slf4j.Slf4j;
+import org.pcap4j.core.*;
+import org.pcap4j.packet.BsdLoopbackPacket;
+import org.pcap4j.packet.IpV4Packet;
+import org.pcap4j.packet.Packet;
+import org.pcap4j.packet.TcpPacket;
+
+import java.io.EOFException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * Created by ydai on 12/10/17.
+ */
+@Slf4j
+public abstract class Pcap4j {
+
+    private static final String networkInterfaceName = "lo0";
+
+    private static int snaplen = 128 * 1024;
+
+    private Thread pcapSnapThread;
+
+    private PcapHandle handle;
+
+    private String host;
+
+    public Pcap4j(String host) {
+        this.host = host;
+
+        handle = openPcap();
+    }
+
+    public void startCapture() {
+        if (handle != null) {
+            runPcapToDispatcher();
+        } else {
+            log.error("No Valid Handle!!!");
+        }
+    }
+
+    private void runPcapToDispatcher() {
+        pcapSnapThread = new Thread() {
+            public void run() {
+                pcapToDispatcher();
+            }
+        };
+
+        pcapSnapThread.start();
+    }
+
+    private synchronized void pcapToDispatcher() {
+        while (true) {
+            try {
+                Packet packet = handle.getNextPacketEx();
+                if (packet != null) {
+                    packet.getHeader();
+                    byte[] rawData = packet.getRawData();
+                    if (rawData.length < 14) {
+                        continue;
+                    }
+
+                    IpV4Packet ipV4Packet = (IpV4Packet) ((BsdLoopbackPacket) packet).getPayload();
+                    TcpPacket tcpPacket = (TcpPacket) ipV4Packet.getPayload();
+
+
+                    // means have more data then header
+                    if (tcpPacket.getPayload() != null) {
+                        String srcAddr = ipV4Packet.getHeader().getSrcAddr().getHostAddress();
+                        String dstAddr = ipV4Packet.getHeader().getDstAddr().getHostAddress();
+                        String srcPort = tcpPacket.getHeader().getSrcPort().valueAsString();
+                        String dstPort = tcpPacket.getHeader().getDstPort().valueAsString();
+                        handleTcpData(srcAddr, dstAddr, srcPort, dstPort, tcpPacket.getPayload());
+                    }
+                }
+            } catch (PcapNativeException | EOFException | TimeoutException e) {
+                continue;
+            } catch (NotOpenException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public abstract void handleTcpData(String srcAddr, String dstAddr, String srcPort, String dstPort, Packet payload);
+
+    private PcapHandle openPcap() {
+        try {
+            PcapNetworkInterface nif = Pcaps.getDevByName(networkInterfaceName);
+            if (nif == null) {
+                throw new RuntimeException("Couldn't open network interface " +
+                        networkInterfaceName);
+            } else {
+                log.info(
+                        "Forward network traffic from {} ({})", nif.getName(), nif.getAddresses());
+            }
+            PcapHandle resultHandle = nif.openLive(snaplen,
+                    PcapNetworkInterface.PromiscuousMode.PROMISCUOUS, 20);
+
+            String filter = "ip and tcp and (src host " + host + ")";
+
+            // 设置过滤器
+            resultHandle.setFilter(filter, BpfProgram.BpfCompileMode.OPTIMIZE);
+            return resultHandle;
+        } catch (PcapNativeException | NotOpenException e) {
+            log.error(
+                    "Couldn't open network interface " + networkInterfaceName,
+                    e);
+            return null;
+        }
+    }
+
+
+}
